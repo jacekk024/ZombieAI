ddec745413e4ef552561afa1bfcc42b3f1905723
TheSkiDY <kk1999skidy@gmail.com>
2022-11-30 00:46:24 +0100

    Player crouching

--- ZombieAI/Assets/Scenes/Scripts/PlayerMove.cs
+++ ZombieAI/Assets/Scenes/Scripts/PlayerMove.cs
@@ -8,38 +8,43 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private bool CanMove = true;
     [SerializeField] private bool CanSprint = true;
     [SerializeField] private bool CanJump = true;
+    [SerializeField] private bool CanCrouch = true;
 
     [Header("Controls")]
     [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
     [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] private KeyCode crouchKey = KeyCode.LeftControl;
 
     [Header("Movement Parameters")]
-    [SerializeField] private float walkSpeed = 10f;
+    [SerializeField] private float walkSpeed = 8f;
     [SerializeField] private float sprintSpeed = 18f;
-    [SerializeField] private float gravity = -9.81f;
-    [SerializeField] private float jumpForce = 8f;
+    [SerializeField] private float crouchSpeed = 4f;
 
+    [Header("Jumping Parameters")]
+    [SerializeField] private float gravity = 9.81f;
+    [SerializeField] private float jumpForce = 2.5f;
+
+    [Header("Crouching Parameters")]
+    [SerializeField] private float crouchHeight = 0.4f;
+    [SerializeField] private float standingHeight = 2f;
+    [SerializeField] private float timeToCrouch = 0.2f;
+    [SerializeField] private Vector3 crouchingCenter = new Vector3(0, 0.5f, 0);
+    [SerializeField] private Vector3 standingCenter = new Vector3(0, 0, 0);
 
     private Vector3 moveDirection;
-    private Vector2 currentInput;
-
     private CharacterController characterController;
-
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public float jumpHeight = 3f;
-    public LayerMask groundMask;
-
-    Vector3 velocity;
-
-    bool isGrounded;
+    private Camera playerCamera;
 
     private bool IsSprinting => CanSprint && Input.GetKey(sprintKey);
-    private bool ShouldJump => Input.GetKey(jumpKey) && characterController.isGrounded;
+    private bool ShouldJump => Input.GetKey(jumpKey) && characterController.isGrounded && !isCrouching;
+    private bool ShouldCrouch => Input.GetKeyDown(crouchKey) && !duringCrouchAnimation && characterController.isGrounded;
+    private bool isCrouching;
+    private bool duringCrouchAnimation;
 
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        playerCamera = GetComponentInChildren<Camera>();
     }
 
     private void HandleMovementInput()
@@ -59,11 +64,49 @@ public class PlayerMove : MonoBehaviour
             moveDirection.y = jumpForce;
     }
 
+    private void HandleCrouch()
+    {
+        if(ShouldCrouch)
+        {
+            StartCoroutine(CrouchStand());
+        }
+    }
+
+    private IEnumerator CrouchStand()
+    {
+        if(isCrouching && Physics.Raycast(playerCamera.transform.position, Vector3.up, 1f)) 
+        {
+            yield break;
+        }
+
+        duringCrouchAnimation = true;
+        float timeElapsed = 0f;
+
+        float targetHeight = isCrouching ? standingHeight : crouchHeight;
+        float currentHeight = characterController.height;
+        Vector3 targetCenter = isCrouching ? standingCenter : crouchingCenter;
+        Vector3 currentCenter = characterController.center;
+
+        while(timeElapsed < timeToCrouch)
+        {
+            characterController.height = Mathf.Lerp(currentHeight, targetHeight, timeElapsed / timeToCrouch);
+            characterController.center = Vector3.Lerp(currentCenter, targetCenter, timeElapsed / timeToCrouch);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        characterController.height = targetHeight;
+        characterController.center = targetCenter;
+
+        isCrouching = !isCrouching;
+        duringCrouchAnimation = false;
+    }
+
     private void ApplyFinalMovements()
     {
         //TO DO: limit player's movements while in mid-air
 
-        float speed = (IsSprinting ? sprintSpeed : walkSpeed);
+        float speed = (isCrouching ? crouchSpeed : (IsSprinting ? sprintSpeed : walkSpeed));
 
         if (!characterController.isGrounded)
             moveDirection.y -= gravity * Time.deltaTime;
@@ -73,7 +116,7 @@ public class PlayerMove : MonoBehaviour
 
         Vector3 moveVector = new Vector3(
             moveDirection.x * speed,
-            moveDirection.y * walkSpeed, //so the player doesn't jump higher while sprinting
+            moveDirection.y * walkSpeed, //so the player always jump the same height
             moveDirection.z * speed
             );
 
@@ -87,6 +130,8 @@ public class PlayerMove : MonoBehaviour
             HandleMovementInput();
             if (CanJump)
                 HandleJump();
+            if (CanCrouch)
+                HandleCrouch();
 
             ApplyFinalMovements();
         }
