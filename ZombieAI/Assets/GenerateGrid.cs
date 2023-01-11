d37f819cef262e5d0dec63ac6a398e7de7247d5c
Dorthion <beleg.dorthion@gmail.com>
2023-01-11 09:16:03 +0100

    Zapis odczyt danych

--- ZombieAI/Assets/GenerateGrid.cs
+++ ZombieAI/Assets/GenerateGrid.cs
@@ -1,17 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GenerateGrid : MonoBehaviour
 {
-    [SerializeField] public static int X;
-    [SerializeField] public static int Y;
-    [SerializeField] public static int Z;
+    [SerializeField] public int X;
+    [SerializeField] public int Y;
+    [SerializeField] public int Z;
     [SerializeField] public int Frequency;
-    [SerializeField] public TextAsset file;
+    // [SerializeField] public TextAsset file;
 
-    private Dane[,,] grid = new Dane[X, Y, Z];
-    private string filepath = "./Assets/Scenes/Resources/data.txt";
+    private Dane[,,] grid;
+    private string filepath = Application.streamingAssetsPath + "/Resources/" + "data.txt"; 
+                                // "./Assets/Scenes/Resources/data.txt";
+    private GameObject groupOfPoints;
 
     internal struct Dane{
         internal int X, Y, Z; // Pozycja zapisana według gridu
@@ -19,37 +25,50 @@ public class GenerateGrid : MonoBehaviour
                               // zombie do gracza, zmiennoprzecinkowy zakres 0-1
     }
 
-    void Start()
+    void Awake()
     {
-        for(int i = 0; i < X; i++)
+        if (CheckFile(filepath))
+        {
+            ReadFile(filepath);
+        }
+
+        grid = new Dane[X, Y, Z];
+        groupOfPoints = new GameObject("Group of Points");
+
+        for (int i = 0; i < X; i++)
         {
             for (int j = 0; j < Y; j++)
             {
                 for (int k = 0; k < Z; k++)
                 {
                     GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                    obj.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-                    obj.transform.position = new Vector3(i + Frequency, j + Frequency, k + Frequency);
+
+                    obj.transform.parent = groupOfPoints.transform;
+                    obj.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                    obj.transform.position = new Vector3( i + (i*Frequency) + gameObject.transform.position.x
+                                                        , j + (j * Frequency) + gameObject.transform.position.y
+                                                        , k + (k * Frequency) + gameObject.transform.position.z );
+                    obj.name = "SpawnPoint;" + (i + (i * Frequency) + gameObject.transform.position.x).ToString() + ";"
+                                             + (j + (j * Frequency) + gameObject.transform.position.y).ToString() + ";"
+                                             + (k + (k * Frequency) + gameObject.transform.position.z).ToString();
                 }
             }
         }
+    }
 
-        // this.grid = new Dane[X, Y, Z];
-
-        if (CheckFile(filepath))
-        {
-            ReadFile(filepath);
-        }
+    private void OnApplicationQuit()
+    {
+        WriteFile(filepath);
     }
 
     void ReadFile(string filename)
     {
-        var lines = file.text.Split(new string[] {"\r\n", "\r", "\n"}, System.StringSplitOptions.RemoveEmptyEntries);
+        List<string> lines = File.ReadLines(filename).ToList();
         string[] line;
 
-        for (int i = 0; i < lines.Length; i++)
+        for (int i = 1; i < lines.Count; i++)
         {
-            line = lines[i].Split(new char[] { ',' }, System.StringSplitOptions.None);
+            line = lines[i].Split(new char[] { ';' }, System.StringSplitOptions.None);
 
             grid[int.Parse(line[0]), int.Parse(line[1]), int.Parse(line[2])].X = int.Parse(line[0]);
             grid[int.Parse(line[0]), int.Parse(line[1]), int.Parse(line[2])].Y = int.Parse(line[1]);
@@ -58,12 +77,48 @@ public class GenerateGrid : MonoBehaviour
         }
     }
 
+    void WriteFile(string filename)
+    {
+        List<string> lines = new List<string>();
+        lines.Add("Scene;Value;X;Y;Z;");
+
+        for (int i = 0; i < X; i++)
+        {
+            for (int j = 0; j < Y; j++)
+            {
+                for (int k = 0; k < Z; k++)
+                {
+                    /*Debug.Log(
+                        SceneManager.GetActiveScene().name.ToString() + ";"
+                      + grid[i, j, k].value.ToString() + ";"
+                      + grid[i, j, k].X.ToString() + ";"
+                      + grid[i, j, k].Y.ToString() + ";"
+                      + grid[i, j, k].Z.ToString() + ";"
+                      );*/
+
+                    lines.Add(
+                        SceneManager.GetActiveScene().name.ToString() + ";"
+                      + grid[i, j, k].value.ToString() + ";"
+                      + grid[i, j, k].X.ToString() + ";"
+                      + grid[i, j, k].Y.ToString() + ";"
+                      + grid[i, j, k].Z.ToString() + ";"
+                      );
+                }
+            }
+        }
+
+        File.WriteAllLines(filename, lines);
+    }
+
     private bool CheckFile(string filename)
     {
+        Debug.Log("CheckFile");
+
         if(!System.IO.File.Exists(filename))
         {
-            System.IO.File.Create(filename);
-            System.IO.File.WriteAllText(filename, "X,Y,Z,Value");
+            Debug.Log("FileNotExists");
+            System.IO.Directory.CreateDirectory(Application.streamingAssetsPath + "/Resources/");
+            System.IO.File.WriteAllText(filename, "Scene;Value;X;Y;Z;\n");
         }
 
         return true;
--- ZombieAI/Assets/WaveSpawner.cs
+++ ZombieAI/Assets/WaveSpawner.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
+    public GameObject generator;
+    public GenerateGrid generateGrid;
+
     [SerializeField] private uint NoWave = 0;
     [SerializeField][Range(1,120)] private uint TimeOfWave;
     [SerializeField][Range(1, 30)] private uint TimeBetweenWaves;
     [SerializeField] private AvaStates State = AvaStates.Spawning;
+
+    public ZombieType[] zombies;
+    public double waveCountdown;
+
+    [System.Serializable]
+    public class ZombieType {
+        public string name;
+        public Transform transform;
+        public uint count;
+        public double rate;
+    }
 
     private enum AvaStates
     {
@@ -16,15 +30,37 @@ public class WaveSpawner : MonoBehaviour
         Waiting     // Przerwa, wolny czas pomiędzy falami
     };
 
-    void Start()
+    void Awake()
     {
         this.NoWave = 1;
+        this.waveCountdown = this.TimeBetweenWaves;
+        this.generateGrid = generator.GetComponent<GenerateGrid>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(this.waveCountdown <= 0)
+        {
+            if(this.State != AvaStates.Spawning )
+            {
+                // Generuj zombie
+                StartCoroutine(SpawnWave()); // Argument jako losowy typ zombie?
+            }
+        } else
+        {
+            this.waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    IEnumerator SpawnWave()
+    {
+        this.State = AvaStates.Spawning;
+
+
+
+        // this.State = AvaStates.Waiting;
+
+        yield break;
     }
 
     private void NextWave() => this.NoWave++;
